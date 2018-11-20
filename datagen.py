9d48108b949c4c3eed5ad99fb18c6d63d593014e
DableUTeeF <palm22180@gmail.com>
2018-11-20 22:12:14 +0700

    Made y 3-class categorical

--- datagen.py
+++ datagen.py
@@ -52,15 +52,21 @@ class Generator(Sequence):
             gt = img.get_fdata()
             gt = np.array(gt, dtype='float32').reshape((*gt.shape, 1)) - 1
             gt = np.rollaxis(gt, 2)
-            return image, gt
+            # todo
+            temp = np.zeros((*gt.shape[:-1], 3), dtype='float32')
+            temp[:, :, :, 0] = gt[:, :, :, 0] == 0 - 1
+            temp[:, :, :, 1] = gt[:, :, :, 0] == 1 - 1
+            temp[:, :, :, 2] = gt[:, :, :, 0] == 2 - 1
+            return image, temp
 
-#
-# if __name__ == '__main__':
-#     from PIL import Image
-#     train_gen = Generator(r'D:\LiTS\Training_Batch2\media\nas\01_Datasets\CT\LITS\Training Batch 2')
-#     x, y = train_gen[0]
-#     print(np.max(x))
-#     xt = np.mean(x.astype('float32').reshape((size, size, x.shape[-2])), axis=-1).astype('int8')
-#     print(np.max(xt))
-#     imx = Image.fromarray(xt, mode='L')
-#     imx.show()
+
+if __name__ == '__main__':
+    from PIL import Image
+    train_gen = Generator(r'D:\LiTS\Training_Batch2\media\nas\01_Datasets\CT\LITS\Training Batch 2')
+    x, y = train_gen[0]
+    # y = np.reshape(y, y.shape[:-1])
+    print(np.max(x))
+    xt = np.mean(x.astype('float32').reshape((size, size, x.shape[-2])), axis=-1).astype('int8')
+    print(np.max(xt))
+    imx = Image.fromarray(xt, mode='L')
+    imx.show()
